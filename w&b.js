2ff987983ba3e5725682f9c38593655c454f1b69
juanpmolinab <111359050+juanpmolinab@users.noreply.github.com>
2025-06-12 12:27:37 +0700

    Update w&b.js

--- w&b.js
+++ w&b.js
@@ -1477,3 +1477,11 @@ window.onload = function() {
 window.location.href = `manifest.html?fuel=${valueFuel}&fuelLand=${valueFuelLand}`;
 
 
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registrado!'))
+    .catch((error) => console.log('Error registrando Service Worker:', error));
+}
+
+
